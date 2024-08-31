292fc2803172f84b733d5273f2385c0decd14a01
Jean Carlos <contato.jeancl25@gmail.com>
2024-08-30 22:32:46 -0300

    feat: hero de apresentação

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import "./Hero.css"
+
+const Hero = () => {
+    return (
+        <section id='hero-banner'>
+            
+            <div className="text-hero">
+                <h1>
+                    <span className='pink-text'>Donuts</span> Artesanais Feitos com <span className='pink-text'>Amor</span>
+                </h1>
+                <p>
+                    Experimente a doçura em cada mordida. Encomende agora e receba em casa!
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default Hero
