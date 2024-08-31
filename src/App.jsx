8dfd0bf82180a576bbbfd1983e731c5f3b293ede
Jean Carlos <contato.jeancl25@gmail.com>
2024-08-30 22:32:14 -0300

    feat: apresentação dos componentes

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Header from './components/Header/Header'
+import Hero from './components/Hero/Hero'
+import Ifood from './components/Ifood/Ifood'
+
+
+function App() {
+
+  return (
+    <>
+      <div className="wrapper">
+        <Header />
+        <Hero />
+        <Ifood />
+      </div>
+    </>
+  )
+}
+
+export default App
